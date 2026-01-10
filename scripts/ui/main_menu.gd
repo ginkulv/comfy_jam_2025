e9e15cee9bbda098d8022e8d33c6ca62ad8132ac
ginkul <geenkool@yandex.ru>
2026-01-10 22:59:28 +0300

    добавил условное меню и настройки

---
+++ scripts/ui/main_menu.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+func _ready():
+    pass
+
+func _process(delta):
+    pass
+
+func _on_exit_button_button_up():
+    get_tree().quit()
+
+func _on_settings_button_button_up():
+    get_tree().change_scene_to_file("res://scenes/ui/settings.tscn")
