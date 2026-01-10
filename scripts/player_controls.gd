8230d56e16ddf0d22192b63f5e333e90bcb04bb4
Aske <klaus.aske@yandex.ru>
2026-01-10 23:21:55 +0300

    Чел ходит, отслеживаем мышь

---
+++ scripts/player_controls.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+
+@export var move_speed : float = 10000
+
+#движение
+func _physics_process(delta):
+	
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * move_speed * delta
+	else:
+		velocity.x = move_toward(velocity.x, 0, move_speed)
+
+	move_and_slide()
+	get_selected_object()
+
+#выбор предметов
+func get_selected_object():
+	var space = get_world_2d().direct_space_state
+	var query = PhysicsPointQueryParameters2D.new()
+	query.position = get_global_mouse_position()
+	query.collide_with_areas = true
+	var intersection = space.intersect_point(query, 1)
+	
+	if intersection.is_empty():
+		return null
+	
+	print (intersection[0].collider)
+	return intersection[0].collider
+	
