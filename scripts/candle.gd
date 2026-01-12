6334df1995814d337788663095d81b388152b616
Aske <klaus.aske@yandex.ru>
2026-01-12 10:07:33 +0300

    Пробелы вместо табов

--- scripts/candle.gd
+++ scripts/candle.gd
@@ -2,4 +2,4 @@ extends StaticBody2D
 
 #зажечься
 func light_up():
-	modulate = Color.RED
+    modulate = Color.RED
--- scripts/ui/settings.gd
+++ scripts/ui/settings.gd
@@ -1,10 +1,10 @@
 extends Node
 
 func _ready():
-	pass
+    pass
 
 func _process(delta):
-	pass
+    pass
 
 func _on_back_button_button_up():
-	get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
+    get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
