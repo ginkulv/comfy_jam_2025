595a912d91b0d0b2014955de50e1bfd15e9bf20b
ginkul <geenkool@yandex.ru>
2026-01-17 22:11:57 +0300

    сделал меню частью основной сцены

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -14,4 +14,4 @@ func _on_settings_button_button_up():
 
 func _on_play_button_button_up() -> void:
     GameState.change_state(GameState.PLAYING)
-    get_tree().change_scene_to_file("res://scenes/levels/level_1.tscn")
+    queue_free()
--- scripts/ui/pause_menu.gd
+++ scripts/ui/pause_menu.gd
@@ -6,12 +6,9 @@ func _on_resume_button_button_up() -> void:
     GameState.change_state(GameState.PLAYING)
     queue_free()
 
-
 func _on_settings_button_button_up() -> void:
     var settings = settings_scene.instantiate()
     add_child(settings)
 
-func _on_go_to_menu_button_button_up() -> void:
-    get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
-    GameState.change_state(GameState.MAIN_MENU)
-    queue_free()
+func _on_exit_button_up() -> void:
+    get_tree().quit()
