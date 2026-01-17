01475a992fa7fb767b7fe48a4c09423675f74f72
Aske <klaus.aske@yandex.ru>
2026-01-17 21:05:04 +0300

    Гном поворачивается туда, куда идет

--- scripts/player_control.gd
+++ scripts/player_control.gd
@@ -6,6 +6,7 @@ extends CharacterBody2D
 @export var left_boundary := -890.0
 @export var right_boundary := 2900.0
 
+@onready var sprite :Sprite2D = $Sprite2D
 func _physics_process(delta):
     
 
@@ -16,6 +17,11 @@ func _physics_process(delta):
         velocity.x = direction * move_speed * delta
     else:
         velocity.x = move_toward(velocity.x, 0, move_speed)
+    
+    if direction > 0:
+        sprite.flip_h = true
+    elif direction < 0:
+        sprite.flip_h = false
 
     move_and_slide()
     
