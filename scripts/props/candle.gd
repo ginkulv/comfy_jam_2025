4e51663ac5a12dd2f3f2fba7b8f8187625b13cd0
Aske <klaus.aske@yandex.ru>
2026-01-17 15:23:53 +0300

    Добавил порталы на все уровни, добавил логику зажигания ламп

--- scripts/props/candle.gd
+++ scripts/props/candle.gd
@@ -1,5 +1,15 @@
-extends StaticBody2D
+extends Area2D
 
+var is_lit : bool = false
+
+func _ready():
+    $".".input_event.connect(_on_area_input_event)
+
+func _on_area_input_event(viewport, event, shape_idx):
+    if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+        if not is_lit:
+            light_up()
 #зажечься
 func light_up():
+    is_lit = true
     modulate = Color.RED
