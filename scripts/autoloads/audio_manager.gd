72b23f981b24b8137b168722c59f3e61c7adb792
ginkul <geenkool@yandex.ru>
2026-01-18 18:56:09 +0300

    добавил в аудиоменеджер несколько каналов для sfx

--- scripts/autoloads/audio_manager.gd
+++ scripts/autoloads/audio_manager.gd
@@ -1,9 +1,13 @@
 extends Node
 
-var bus_names = ["SFX", "Music" ]
+const max_sfx_players: int = 10
+
+var bus_names = ["SFX", "Music"]
+
 var music_player: AudioStreamPlayer
-var sfx_player: AudioStreamPlayer
-var bus_index : int
+var sfx_players: Array[AudioStreamPlayer]
+
+var bus_index: int
 
 @export var music_volume_ln : float = 80
 @export var sfx_volume_ln : float = 80
@@ -27,25 +31,42 @@ func _ready() -> void:
     music_player.bus = "Music"
     add_child(music_player)
 
-    sfx_player = AudioStreamPlayer.new()
-    sfx_player.bus = "SFX"
-    add_child(sfx_player)
+    for i in range(max_sfx_players):
+        var sfx_player = AudioStreamPlayer.new()
+        sfx_player.bus = "SFX"
+        sfx_players.append(sfx_player)
+        add_child(sfx_player)
 
-func play_sfx(sfx_name_with_extension: String) -> void:
-    sfx_player.stream = load("res://assets/sfx/" + sfx_name_with_extension)
-    sfx_player.play()
+func play_sfx(sfx_name_with_extension: String, loop: bool = false) -> void:
+    var stream = load("res://assets/sfx/" + sfx_name_with_extension)
+    stream.loop = loop
 
-func play_music(music_name_with_extension: String) -> void:
+    for player in sfx_players:
+        if !player.playing:
+            player.stream = stream
+            player.play()
+            break
+
+func stop_sfx(sfx_name_with_extension: String) -> void:
+    for player in sfx_players:
+        if not player.stream:
+            continue
+        if player.stream.resource_path.get_file() == sfx_name_with_extension:
+            player.stop()
+
+func play_music(music_name_with_extension: String, loop: bool = true) -> void:
     var stream = load("res://assets/music/" + music_name_with_extension)
-    stream.loop = true
+    stream.loop = loop
     music_player.stream = stream
     music_player.play()
+
+func stop_music():
+    music_player.stop()
 
 func change_sfx_volume(volume: float) -> void:
     sfx_volume_ln = volume
     bus_index = AudioServer.get_bus_index("SFX")
     AudioServer.set_bus_volume_linear(bus_index, volume / 100)
-
 
 func change_music_volume(volume: float) -> void:
     music_volume_ln = volume
