89dea5821f2ba2d4773d5a7addbf2c3ae4c3a064
Aske <klaus.aske@yandex.ru>
2026-01-20 12:04:19 +0300

    Еще немного про свечи

--- scripts/props/candle_control.gd
+++ scripts/props/candle_control.gd
@@ -2,6 +2,18 @@ extends Control
 
 @onready var candle = $".."
 
+func _gui_input(event):
+    if GameState.input_locked == true:
+        return
+        
+    if event is InputEventMouseButton \
+    and event.pressed \
+    and event.button_index == MOUSE_BUTTON_LEFT:
+        if not InventoryManager.has_item(Items.Id.MATCHES):
+            return
+        
+        candle.light_up()
+
 func _can_drop_data(_at_position: Vector2, data: Variant) -> bool:
     return data.item_id == Items.Id.MATCHES
     # return true
