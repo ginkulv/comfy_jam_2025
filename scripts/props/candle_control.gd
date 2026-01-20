016b8042ee1fe158e8ab681d6cdd8c851c227e91
Aske <klaus.aske@yandex.ru>
2026-01-20 11:07:21 +0300

    Маленькие переработки логики свечей

--- scripts/props/candle_control.gd
+++ scripts/props/candle_control.gd
@@ -2,9 +2,20 @@ extends Control
 
 @onready var candle = $".."
 
+func _gui_input(event):
+    if event is InputEventMouseButton \
+    and event.pressed \
+    and event.button_index == MOUSE_BUTTON_LEFT:
+        
+        if not InventoryManager.has_item(Items.Id.MATCHES):
+            return
+        candle.light_up()
+        
 func _can_drop_data(_at_position: Vector2, data: Variant) -> bool:
     return data.item_id == Items.Id.MATCHES
     # return true
 
 func _drop_data(_at_position: Vector2, data: Variant) -> void:
-    candle.light_up()
+    if candle.is_lit == false:
+          candle.light_up()
+  
