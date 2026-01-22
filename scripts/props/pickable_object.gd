b41206b3d7ad94029300a482b964e7000de6aeb0
ilKogan <vinsent.in7aider@gmail.com>
2026-01-22 04:19:19 +0300

    добавил sfx_pickitem.wav в скрипт предметов

--- scripts/props/pickable_object.gd
+++ scripts/props/pickable_object.gd
@@ -4,24 +4,24 @@ var mouse_over : bool = false
 @export var item_id : Items.Id
 
 func _ready():
-    mouse_entered.connect(_on_mouse_entered)
-    mouse_exited.connect(_on_mouse_exited)
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
 
 func _on_mouse_entered():
-    mouse_over = true
+	mouse_over = true
 
 func _on_mouse_exited():
-    mouse_over = false
+	mouse_over = false
 
 func _input(event):
-    if GameState.input_locked:
-        return
-    
-    if mouse_over and event is InputEventMouseButton:
-        if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-            on_left_click()
-            
+	if GameState.input_locked:
+		return
+	
+	if mouse_over and event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			on_left_click()
+			
 func on_left_click():
-    InventoryManager.add_item(item_id)
-    AudioManager.play_sfx("click" + ".mp3")
-    queue_free()
+	InventoryManager.add_item(item_id)
+	AudioManager.play_sfx("sfx_pickitem.wav")
+	queue_free()
