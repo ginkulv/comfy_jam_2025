75ef39d17a59bdbe5153922f9ed58d1f1b468597
ginkul <geenkool@yandex.ru>
2026-01-23 12:52:10 +0300

    добавил звук глухой метели

--- scripts/window_area.gd
+++ scripts/window_area.gd
@@ -1,5 +1,8 @@
 extends Area2D
 
+func _ready() -> void:
+    AudioManager.play_sfx("sfx_blizzard.wav")
+
 func _input_event(_viewport, event, shape_idx):
     if cannot_interact() or GameState.input_locked:
         return
@@ -9,6 +12,8 @@ func _input_event(_viewport, event, shape_idx):
     and event.button_index == MOUSE_BUTTON_LEFT:
         GameState.set_flag("window_closed")
         AudioManager.play_sfx("closing_window.wav")
+        AudioManager.stop_sfx("sfx_blizzard.wav")
+        AudioManager.play_sfx("sfx_blizzard_shut.wav")
              
             
 func cannot_interact() -> bool:
