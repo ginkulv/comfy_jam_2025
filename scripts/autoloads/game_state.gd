51888b35ecf4a5412a54dce447d82013d8832f51
ilKogan <vinsent.in7aider@gmail.com>
2026-01-23 05:40:16 +0300

    Добавил катсцену c текстом, метель, немного поменял кое где гейм стейты и глобалсвет, фиксы лампы

--- scripts/autoloads/game_state.gd
+++ scripts/autoloads/game_state.gd
@@ -8,10 +8,10 @@ var key_allowed : bool = false
 var xylophone_allowed : bool = false
 var flags := {}
 enum {
-    MAIN_MENU,
-    PAUSE_MENU,
-    PLAYING,
-    CUTSCENE,
+	MAIN_MENU,
+	PAUSE_MENU,
+	PLAYING,
+	CUTSCENE,
 }
 
 signal state_changed(from_state: int, to_state: int)
@@ -20,32 +20,32 @@ var current_state: int = MAIN_MENU
 var pause_menu: Node2D
 
 func _ready() -> void:
-    process_mode = Node.PROCESS_MODE_ALWAYS
-    change_state(MAIN_MENU)
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	change_state(MAIN_MENU)
 
 func change_state(new_state: int) -> void:
-    state_changed.emit(current_state, new_state)
-    current_state = new_state
+	state_changed.emit(current_state, new_state)
+	current_state = new_state
 
-    match current_state:
-        PLAYING:
-            get_tree().paused = false
-        PAUSE_MENU, CUTSCENE, MAIN_MENU:
-            get_tree().paused = true
+	match current_state:
+		PLAYING,CUTSCENE:
+			get_tree().paused = false
+		PAUSE_MENU, MAIN_MENU:
+			get_tree().paused = true
 
 func _input(event: InputEvent) -> void:
-    if event.is_action_pressed("toggle_pause_menu"):
-        match current_state:
-            PLAYING:
-                pause_menu = pause_menu_scene.instantiate()
-                add_child(pause_menu)
-                change_state(PAUSE_MENU)
-            PAUSE_MENU:
-                pause_menu.queue_free()
-                change_state(PLAYING)
+	if event.is_action_pressed("toggle_pause_menu"):
+		match current_state:
+			PLAYING:
+				pause_menu = pause_menu_scene.instantiate()
+				add_child(pause_menu)
+				change_state(PAUSE_MENU)
+			PAUSE_MENU:
+				pause_menu.queue_free()
+				change_state(PLAYING)
 
 func set_flag(flag: String) -> void:
-    flags[flag] = true
+	flags[flag] = true
 
 func has_flag(flag: String) -> bool:
-    return flags.get(flag, false)
+	return flags.get(flag, false)
---
+++ scripts/ui/cutscene_fadin_text.gd
@@ -0,0 +1,40 @@
+extends RichTextLabel
+
+signal fade_completed
+
+@export var next_button: Button
+@export var fade_duration: float = 3.0
+@export var fade_start_offset: float = -20.0
+@export var fade_length: int = 10
+@export var fade_button_start: float = 3
+
+func _ready() -> void:
+	start_fade_effect()
+
+func start_fade_effect():
+	var tween = create_tween()
+	var start_value = fade_start_offset
+	var end_value = get_total_character_count() + fade_length
+	
+	if next_button:
+		next_button.visible = false
+		next_button.disabled = true
+	
+	tween.tween_method(_apply_fade, start_value, end_value, fade_duration)
+	tween.finished.connect(_on_tween_finished)
+
+func _apply_fade(value: float):
+	var current_bbcode: String = "[fade start=" + str(int(value)) + " length=" + str(fade_length) + "]" + text + "[/fade]"
+	parse_bbcode(current_bbcode)
+
+func _on_tween_finished():
+	parse_bbcode(text)  
+	
+	if next_button:
+		next_button.visible = true
+		next_button.disabled = false
+		var button_tween = create_tween()
+		next_button.modulate = Color.TRANSPARENT
+		button_tween.tween_property(next_button, "modulate", Color.WHITE,fade_button_start)
+	
+	emit_signal("fade_completed")
