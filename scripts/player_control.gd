98e0b32c29aa14ddbafa01c90428647f20d603d1
Aske <klaus.aske@yandex.ru>
2026-01-23 20:09:10 +0300

    Поменял анимацию гнома

--- scripts/player_control.gd
+++ scripts/player_control.gd
@@ -6,8 +6,8 @@ extends CharacterBody2D
 @export var left_boundary := -890.0
 @export var right_boundary := 2900.0
 
+@onready var animated_sprite = $AnimatedSprite2D
 #@onready var sprite :Sprite2D = $Sprite2D
-@onready var dragon_bones_armature :DragonBonesArmatureView = $DragonBonesArmatureView
 @onready var left_light:PointLight2D = $left_light
 @onready var right_light:PointLight2D = $right_light
 @onready var left_light_2:PointLight2D = $left_light2
@@ -22,22 +22,24 @@ func _physics_process(delta):
 
     if direction:
         velocity.x = direction * move_speed * delta
-        dragon_bones_armature.current_animation = "walk"
+        animated_sprite.play("walk")
     else:
         velocity.x = move_toward(velocity.x, 0, move_speed)
-        dragon_bones_armature.current_animation = "idle"
+        animated_sprite.play("idle")
 
     if direction > 0:
         on_flip = true
     elif direction < 0:
         on_flip = false
 
-    dragon_bones_armature.flip_x = on_flip
+    
+    animated_sprite.flip_h = on_flip
     right_light.visible = on_flip
     right_light_2.visible = on_flip
     left_light.visible = !on_flip
     left_light_2.visible = !on_flip
 
     move_and_slide()
+    
 
     global_position.x = clamp(global_position.x, left_boundary, right_boundary)
