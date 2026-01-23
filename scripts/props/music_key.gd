53b6e9f529587fce0ae1ed619757070db8ed3d87
Aske <klaus.aske@yandex.ru>
2026-01-24 01:14:50 +0300

    Добавил финальную катсцену

--- scripts/props/music_key.gd
+++ scripts/props/music_key.gd
@@ -8,24 +8,24 @@ extends Area2D
 
 
 func _input_event(_viewport, event, _shape_idx):
-	if can_play() == false:
-		return
-	
-	if event is InputEventMouseButton \
-	and event.pressed \
-	and event.button_index == MOUSE_BUTTON_LEFT:
-		puzzle.register_note(note_id)
-		var note_name = str(note_id)
-		AudioManager.play_sfx("sfx_xylophone_" + note_name + ".wav", 1.4)
-		if not note_played():
-			GameState.set_flag("note_played")
-			MessageManager.display_text_by_id("xylophone_start")
-	   
-		print(note_name)
-		
+    if can_play() == false:
+        return
+    
+    if event is InputEventMouseButton \
+    and event.pressed \
+    and event.button_index == MOUSE_BUTTON_LEFT:
+        puzzle.register_note(note_id)
+        var note_name = str(note_id)
+        AudioManager.play_sfx("sfx_xylophone_" + note_name + ".wav", 1.4)
+        if not note_played():
+            GameState.set_flag("note_played")
+            MessageManager.display_text_by_id("xylophone_start")
+       
+        print(note_name)
+        
    
 func can_play() -> bool:
-	return GameState.has_flag(required_flag)
+    return GameState.has_flag(required_flag)
  
 func note_played() -> bool:
-	return GameState.has_flag(flag_for_message)
+    return GameState.has_flag(flag_for_message)
--- scripts/ui/final_cutscene.gd
+++ scripts/ui/final_cutscene.gd
@@ -1,1 +1,15 @@
-extends AnimationPlayer
+extends Node
+
+@onready var anim = $AnimationPlayer
+
+func play():
+    anim.play("cutscene")
+
+    
+
+func quit_game():
+    get_tree().quit()
+
+
+func _on_music_puzzle_on_winning_the_game() -> void:
+    play()
