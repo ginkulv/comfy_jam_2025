df727ef5b9925449e5004aeee2af60ac0859f428
ilKogan <vinsent.in7aider@gmail.com>
2026-01-25 02:07:43 +0300

    Фикс мердж сцены

--- scripts/props/fireplace_control.gd
+++ scripts/props/fireplace_control.gd
@@ -7,14 +7,6 @@ func _can_drop_data(_at_position: Vector2, data: Variant) -> bool:
 	# return true
 
 func _drop_data(_at_position: Vector2, data: Variant) -> void:
-<<<<<<< HEAD
-	InventoryManager.remove_item(data.item_id)
-	AudioManager.play_sfx("sfx_fireplace.wav")
-	GameState.set_flag("room_unfrozen")
-	AudioManager.add_layer()
-	MessageManager.display_text_by_id("fireplace_lit")
-	anim.visible = true
-=======
     InventoryManager.remove_item(data.item_id)
     AudioManager.play_sfx("sfx_fireplace.mp3", 0.8)
     GameState.set_flag("room_unfrozen")
@@ -22,7 +14,6 @@ func _drop_data(_at_position: Vector2, data: Variant) -> void:
     AudioManager.add_layer()
     MessageManager.display_text_by_id("fireplace_lit")
     anim.visible = true
->>>>>>> 2e993ce3cbdfa79e1b5080031aa4db3505143ca3
 
 func _gui_input(event):
 	if event is InputEventMouseButton \
