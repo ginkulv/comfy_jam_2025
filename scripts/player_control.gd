487da36911d7b2df8880cc7d8ee271fdd33b591c
Aske <klaus.aske@yandex.ru>
2026-01-25 22:54:14 +0300

    Микрофиксы

--- scripts/player_control.gd
+++ scripts/player_control.gd
@@ -15,30 +15,30 @@ extends CharacterBody2D
 func _physics_process(delta):
 
 
-	var direction = Input.get_axis("walk_letf", "walk_right")
-	
-	if GameState.input_locked:
-		return
+    var direction = Input.get_axis("walk_letf", "walk_right")
+    
+    if GameState.input_locked:
+        return
 
 
-	if direction:
-		velocity.x = direction * move_speed * delta
-		animated_sprite.play("walk")
-	else:
-		velocity.x = move_toward(velocity.x, 0, move_speed)
-		animated_sprite.play("idle")
+    if direction:
+        velocity.x = direction * move_speed * delta
+        animated_sprite.play("walk")
+    else:
+        velocity.x = move_toward(velocity.x, 0, move_speed)
+        animated_sprite.play("idle")
 
-	if direction > 0:
-		on_flip = true
-	elif direction < 0:
-		on_flip = false
+    if direction > 0:
+        on_flip = true
+    elif direction < 0:
+        on_flip = false
 
-	
-	animated_sprite.flip_h = on_flip
-	right_light.visible = on_flip
-	left_light.visible = !on_flip
+    
+    animated_sprite.flip_h = on_flip
+    right_light.visible = on_flip
+    left_light.visible = !on_flip
 
-	move_and_slide()
-	
+    move_and_slide()
+    
 
-	global_position.x = clamp(global_position.x, left_boundary, right_boundary)
+    global_position.x = clamp(global_position.x, left_boundary, right_boundary)
