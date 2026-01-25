153e0445d1d44be291a4c6bba2d43e157f71be75
ginkul <geenkool@yandex.ru>
2026-01-25 17:11:15 +0300

    добавил затихание музыки в финальной катсцене

--- scripts/ui/final_cutscene.gd
+++ scripts/ui/final_cutscene.gd
@@ -4,14 +4,14 @@ extends Node
 @onready var anim = $AnimationPlayer
 
 func play():
-	#layer.visible = true
-	anim.play("win")
+    #layer.visible = true
+    anim.play("win")
 
-	
+    
 
 func quit_game():
-	get_tree().quit()
+    get_tree().quit()
 
 
 func _on_music_puzzle_on_winning_the_game() -> void:
-	play()
+    play()
