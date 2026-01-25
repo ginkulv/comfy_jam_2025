aa1bbd7eb8286ee3734d2949543a1848a35af8ce
Aske <klaus.aske@yandex.ru>
2026-01-25 17:42:17 +0300

    Для пуша в master

--- scripts/props/window_area.gd
+++ scripts/props/window_area.gd
@@ -1,34 +1,34 @@
 extends Area2D
-	
+    
 var window_closed : bool = false
 func _ready() -> void:
-	AudioManager.play_sfx("sfx_blizzard.wav")
-	AudioManager.play_sfx("window_open.wav", 0.6)
-	get_node("Node2D/open").visible = true 
-	get_node("Node2D/close").visible = false
+    AudioManager.play_sfx("sfx_blizzard.wav")
+    AudioManager.play_sfx("window_open.wav", 0.6)
+    get_node("Node2D/open").visible = true 
+    get_node("Node2D/close").visible = false
 
 func _input_event(_viewport, event, _shape_idx):
-	if GameState.input_locked:
-		return
-	
-	if window_closed:
-		#MessageManager.display_text_by_id("window_after")
-		return
-	
-	if event is InputEventMouseButton \
-	and event.pressed \
-	and event.button_index == MOUSE_BUTTON_LEFT:
-		get_node("Node2D/open").visible = false 
-		get_node("Node2D/close").visible = true 
-		GameState.set_flag("window_closed")
-		AudioManager.play_sfx("closing_window.wav")
-		await get_tree().create_timer(0.8).timeout
-		AudioManager.stop_sfx("sfx_blizzard.wav")
-		AudioManager.stop_sfx("window_open.wav")
-		AudioManager.play_sfx("sfx_blizzard_shut.wav", 0.1)
-		window_closed = true
-		$snow2.emitting = false
-		
-		await get_tree().create_timer(0.5).timeout
-		MessageManager.display_text_by_id("window_closed")
-			 
+    if GameState.input_locked:
+        return
+    
+    if window_closed:
+        #MessageManager.display_text_by_id("window_after")
+        return
+    
+    if event is InputEventMouseButton \
+    and event.pressed \
+    and event.button_index == MOUSE_BUTTON_LEFT:
+        get_node("Node2D/open").visible = false 
+        get_node("Node2D/close").visible = true 
+        GameState.set_flag("window_closed")
+        AudioManager.play_sfx("closing_window.wav")
+        await get_tree().create_timer(0.8).timeout
+        AudioManager.stop_sfx("sfx_blizzard.wav")
+        AudioManager.stop_sfx("window_open.wav")
+        AudioManager.play_sfx("sfx_blizzard_shut.wav", 0.1)
+        window_closed = true
+        $snow2.emitting = false
+        
+        await get_tree().create_timer(0.5).timeout
+        MessageManager.display_text_by_id("window_closed")
+             
