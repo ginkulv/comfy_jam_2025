3155c38ea061826472b5a281c8627e23d9bd0218
Aske <klaus.aske@yandex.ru>
2026-01-26 00:56:26 +0300

    Изменения коллизий, правки логики

--- scripts/snow_manager.gd
+++ scripts/snow_manager.gd
@@ -7,15 +7,18 @@ var snow_list : Array = []
 var current_index : int = 0
 
 func _ready():
-	snow_list = get_tree().get_nodes_in_group("snow")
-	snow_list.sort_custom(func(a, b):
-		return a.snow_id < b.snow_id
-	)
-	GameState.OnFireplace.connect(start_melting)
+    snow_list = get_tree().get_nodes_in_group("snow")
+    snow_list.sort_custom(func(a, b):
+        return a.snow_id < b.snow_id
+        )
+        
+    GameState.OnFireplace.connect(start_melting)
+
 
 func start_melting():
-	for i in snow_list.size():
-		melt_with_delay(snow_list[i], i)
+    for i in snow_list.size():
+        melt_with_delay(snow_list[i], i)
+
 
 
 func melt_with_delay(snow, index: int):
