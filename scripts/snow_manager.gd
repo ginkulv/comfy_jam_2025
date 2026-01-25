40f5ae0a15b43e73570dad922b55ba1440fe9158
ilKogan <vinsent.in7aider@gmail.com>
2026-01-26 00:21:57 +0300

    Fix snow

--- scripts/snow_manager.gd
+++ scripts/snow_manager.gd
@@ -1,28 +1,22 @@
 extends Node
 
-@export var melt_time := 0.45
-@export var wave_delay := 0.05
+@export var melt_time := 0.75
+@export var wave_delay := 0.20
 
 var snow_list : Array = []
 var current_index : int = 0
 
 func _ready():
-    snow_list = get_tree().get_nodes_in_group("snow")
-    snow_list.sort_custom(func(a, b):
-        return a.snow_id < b.snow_id
-    )
-    GameState.OnFireplace.connect(start_melting)
+	snow_list = get_tree().get_nodes_in_group("snow")
+	snow_list.sort_custom(func(a, b):
+		return a.snow_id < b.snow_id
+	)
+	GameState.OnFireplace.connect(start_melting)
 
 func start_melting():
-    current_index = 0
-    melt_next()
+	for i in snow_list.size():
+		melt_with_delay(snow_list[i], i)
 
-func melt_next():
-    if current_index >= snow_list.size():
-        return
-
-    var snow = snow_list[current_index]
-    current_index += 1
 
 func melt_with_delay(snow, index: int):
 	var tween := create_tween()
